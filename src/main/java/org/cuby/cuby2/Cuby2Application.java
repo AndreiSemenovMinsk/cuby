c74a3aa98fb430033d6bea356cb1832ebc067ecf
As <35848348+AndreiSemenovMinsk@users.noreply.github.com>
2025-11-24 11:39:37 +0300

    Initial commit

---
+++ src/main/java/org/cuby/cuby2/Cuby2Application.java
@@ -0,0 +1,13 @@
+package org.cuby.cuby2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuby2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuby2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cuby/cuby2/Cuby2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cuby.cuby2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cuby2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
